509cba051a409a1c25aa66ff0acdf0ad7df003f2
Gaelle <gaelle@bimdata.io>
2023-01-19 15:50:50 +0100

    create dev folder

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import BIMDataDesignSystem from "@bimdata/design-system/plugin-vue3.js";
-import App from "./App.vue";
-
-const app = createApp(App)
-  .use(BIMDataDesignSystem())
-
-app.mount("#app");
