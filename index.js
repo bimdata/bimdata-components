8cd4b30587e6148308baf15f9013c3e93bad1b1a
NicolasRichel <nicolas@bimdata.io>
2024-03-29 16:58:27 +0100

    MINOR: feat: add BIMDataTree component

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ export { default as BIMDataGuidedTour } from "./src/BIMDataGuidedTour/BIMDataGui
 export { default as BIMDataModelPreview } from "./src/BIMDataModelPreview/BIMDataModelPreview.vue";
 export { default as BIMDataPDFViewer } from "./src/BIMDataPDFViewer/BIMDataPDFViewer.vue";
 export { default as BIMDataSafeZoneModal } from "./src/BIMDataSafeZoneModal/BIMDataSafeZoneModal.vue";
+export { default as BIMDataTree } from "./src/BIMDataTree/BIMDataTree.vue";
